2eb4d4fbbc3771b33c51c7dc7e6d8fad2395e0fc
Koen Kamman <koen@kamman.dev>
2026-03-19 20:16:49 +0100

    chore: initial commit

---
+++ Stratego.Core.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Stratego.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Stratego.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Stratego.Core
+{
+    public class Class1
+    {
+
+    }
+}
